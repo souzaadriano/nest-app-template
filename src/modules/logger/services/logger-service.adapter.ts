2b5e0b327e64d067f7cafb4640587f6875dd8472
Adriano Souza <souzaadriano.contato@gmail.com>
2023-09-24 22:09:04 -0300

    feat(logger-service.adapter): create a contract to logger-service

--- src/modules/logger/services/logger-service.adapter.ts
+++ src/modules/logger/services/logger-service.adapter.ts
@@ -1,27 +1,22 @@
 import { Log } from '@/core/shared/class/log/log.class';
-import { AbstractException } from '@/core/shared/exception/exception.abstract';
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { Logger } from 'pino';
+import { ILoggerService } from './logger-service.contract';
 import { PinoFactory } from './pino.factory';
 
 @Injectable()
-export class LoggerService implements OnModuleInit {
+export class LoggerService implements OnModuleInit, ILoggerService {
   private _pino: Logger;
 
   onModuleInit() {
     this._pino = PinoFactory.factory();
   }
 
-  emit(context: string, message: string) {
+  text(context: string, message: string) {
     this._pino.info(`[${context}] ${message}`);
   }
 
   log(log: Log) {
     this._pino.info(`[${log.context}]`, log.data());
-  }
-
-  exception(log: Log, exception: AbstractException) {
-    log.exception(exception);
-    this.log(log);
   }
 }
